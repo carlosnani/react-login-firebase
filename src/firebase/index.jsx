64111f94efe893ef127a2a2359fb6b42e3bea911
Crlos Nani <carlosnani80@gmail.com>
2022-06-20 23:09:31 -0300

    test_deploy

--- src/firebase/index.jsx
+++ src/firebase/index.jsx
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
